45748056b30647cc9159f4cce37fa777dcea407e
Ivin <ivinmk2410@hotmail.com>
2024-12-08 22:30:52 +0530

    Day 8 Solutions

---
+++ day8/day8B.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+public class day8B {
+    public static void main(String[] args) {
+        List<List<Character>> input = new ArrayList<>();
+        Map<Character, List<Integer[]>> map = new HashMap<>();
+
+        int rows = 0;
+        int cols = 0;
+
+        try(BufferedReader br = new BufferedReader(new FileReader("day8input.txt"))){
+            String line;
+            rows = 0;
+            while((line=br.readLine())!=null){
+                char[] temp = line.toCharArray();
+                List<Character> points = new ArrayList<>();
+                cols=0;
+
+                for(char c : temp){
+                    points.add(c);
+                    if(c!='.'){
+                        if(map.containsKey(c)){
+                            map.get(c).add(new Integer[]{rows,cols});
+                        }
+                        else{
+                            List<Integer[]> position = new ArrayList<>();
+                            position.add(new Integer[]{rows,cols});
+                            map.put(c, position);
+                        }
+                    }
+                    cols++;
+                }
+                input.add(points);
+                rows++;
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        int count = 0;
+        List<List<Character>> antinodes = input;
+        for(Character key : map.keySet()){
+            List<Integer[]> points = map.get(key);
+            for(Integer[] p1 : points){
+                for(Integer[] p2 : points){
+                    if(p1==p2)
+                        continue;
+                    else{
+                        Integer[] diff = new Integer[]{p1[0]-p2[0],p1[1]-p2[1]};
+                        Integer[] new1 = new Integer[]{p1[0],p1[1]};
+                        Integer[] new2 = new Integer[]{p2[0],p2[1]};
+
+                        while(new1[0]<rows && new1[0]>=0 && new1[1]<cols && new1[1]>=0){
+                            if(antinodes.get(new1[0]).get(new1[1])!='#'){
+                                antinodes.get(new1[0]).set(new1[1],'#');
+                                count++;
+                            }
+                            new1[0]+=diff[0];
+                            new1[1]+=diff[1];
+                        }
+
+                        while(new2[0]<rows && new2[0]>=0 && new2[1]<cols && new2[1]>=0){
+                            if(antinodes.get(new2[0]).get(new2[1])!='#'){
+                                antinodes.get(new2[0]).set(new2[1],'#');
+                                count++;
+                            }
+                            new2[0]-=diff[0];
+                            new2[1]-=diff[1];
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Total count = "+count);
+    }
+}
